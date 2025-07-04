9dba2551ab48e3a8f02d626db977c712ce51df99
David Bihako <davidbihako4@gmail.com>
2025-07-04 13:48:54 +0200

    Add PATCH /users/:id/role route for promoting and demoting user roles

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,8 @@
 import { listUsers } from '../controllers/listUsers.js'
+import { updateUserRole } from '../controllers/updateUserRole.js'
 
 export default async function (app) {
   // Only ADMIN and SUPERUSER can list users
   app.get('/users', { preValidation: [app.authenticate] }, listUsers)
+  app.patch('/users/:id/role', { preValidation: [app.authenticate] }, updateUserRole)
 }
